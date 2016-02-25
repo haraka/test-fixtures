137ef1cbea8fd633803f74135c1f0251ce4c4103
Matt Simerson <matt@tnpi.net>
2016-02-24 22:27:18 -0800

    better path-to-plugin resolution

--- test/index.js
+++ test/index.js
@@ -1,42 +1,54 @@
 'use strict';
 
-var index = require('../index');
-// console.log(index);
+var fixtures = require('../index');
 
 exports.exports = {
     'stub': function (test) {
         test.expect(1);
-        test.equal(typeof index.stub.stub, 'function');
+        test.equal(typeof fixtures.stub.stub, 'function');
         test.done();
     },
     'logger': function (test) {
         test.expect(1);
-        test.equal(typeof index.logger.loginfo, 'function');
+        test.equal(typeof fixtures.logger.loginfo, 'function');
         test.done();
     },
     'connection': function (test) {
         test.expect(1);
-        test.equal(typeof index.connection.createConnection, 'function');
+        test.equal(typeof fixtures.connection.createConnection, 'function');
         test.done();
     },
     'transaction': function (test) {
         test.expect(1);
-        test.equal(typeof index.transaction.createTransaction, 'function');
+        test.equal(typeof fixtures.transaction.createTransaction, 'function');
         test.done();
     },
     'line_socket': function (test) {
         test.expect(1);
-        test.equal(typeof index.line_socket.connect, 'function');
+        test.equal(typeof fixtures.line_socket.connect, 'function');
+        test.done();
+    },
+    'plugin': function (test) {
+        test.expect(1);
+        var p = fixtures.plugin('test/fixtures/mock-plugin');
+        test.equal(typeof p.load_plugin, 'function');
+        test.done();
+    },
+    'result_store': function (test) {
+        test.expect(1);
+        var rs = new fixtures.result_store();
+        // console.log(rs);
+        test.equal(typeof rs.add, 'function');
         test.done();
     },
     'util_hmailitem': function (test) {
         test.expect(1);
-        test.equal(typeof index.util_hmailitem.newMockHMailItem, 'function');
+        test.equal(typeof fixtures.util_hmailitem.newMockHMailItem, 'function');
         test.done();
     },
     'vm_harness': function (test) {
         test.expect(1);
-        test.equal(typeof index.vm_harness.sandbox_require, 'function');
+        test.equal(typeof fixtures.vm_harness.sandbox_require, 'function');
         test.done();
     }
 }
