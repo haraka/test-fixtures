dceb4cc941947905bf460ccdd0c2e4163f3a2428
Matt Simerson <matt@tnpi.net>
2016-02-28 10:30:17 -0800

    enable more Haraka tests to depend on this

--- test/transaction.js
+++ test/transaction.js
@@ -16,4 +16,18 @@ exports.transaction = {
         test.ok(newTrans);
         test.done();
     },
+    'can add a header': function (test) {
+        test.expect(1);
+        var newTrans = transaction.createTransaction();
+        newTrans.add_header('X-Test-Header', 'Has a value');
+        test.ok(true);  // previous function didn't throw
+        test.done();
+    },
+    'can call message_stream.add_line': function (test) {
+        test.expect(1);
+        var newTrans = transaction.createTransaction();
+        //console.log(newTrans.message_stream);
+        test.ok(newTrans.message_stream.add_line('foo\r\n'));
+        test.done();
+    },
 }
