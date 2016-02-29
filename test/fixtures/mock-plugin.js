ce55b11e4ea86c85da46ad69ad83bbf91b0fea9b
Matt Simerson <matt@tnpi.net>
2016-02-28 22:01:31 -0800

    inheritance tests and package support

--- test/fixtures/mock-plugin.js
+++ test/fixtures/mock-plugin.js
@@ -1,5 +1,5 @@
 
-
 exports.register = function () {
-
+    // console.log('I am a mock plugin file');
+    this.inherits('mock-plugin-dir');
 };
--- test/plugin.js
+++ test/plugin.js
@@ -9,11 +9,49 @@ exports.Plugin = {
         test.equal(typeof Plugin, 'function');
         test.done();
     },
-    'creates a new Plugin': function (test) {
+    'creates a new Plugin from .js': function (test) {
         test.expect(1);
         var newPlugin = new Plugin('test/fixtures/mock-plugin');
         // console.log(newPlugin);
         test.ok(newPlugin);
+        test.done();
+    },
+    'creates a new Plugin from dir': function (test) {
+        test.expect(1);
+        var newPlugin = new Plugin('test/fixtures/mock-plugin-dir');
+        // console.log(newPlugin);
+        test.ok(newPlugin);
+        test.done();
+    },
+}
+
+exports.contents = {
+    setUp: function (done) {
+        // console.log(Plugin);
+        this.plugin = new Plugin('test/fixtures/mock-plugin-dir');
+        done();
+    },
+    'register exists': function (test) {
+        test.expect(1);
+        // console.log(this.plugin);
+        test.equal(typeof this.plugin.register, 'function');
+        test.done();
+    },
+    'register runs': function (test) {
+        test.expect(1);
+        this.plugin.register();
+        test.ok(true); // register() didn't throw
+        test.done();
+    }
+}
+
+exports.inherits = {
+    'can register plugin with ineritance': function (test) {
+        test.expect(2);
+        var pi = new Plugin('test/fixtures/mock-plugin');
+        test.equal(typeof pi.register, 'function');
+        pi.register();
+        test.ok(Object.keys(pi.base));
         test.done();
     },
 }
