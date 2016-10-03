d9bf341789e20d16702db059646cbe27dee5e738
Matt Simerson <matt@tnpi.net>
2016-10-03 10:36:16 -0700

    more functional header add/get/rm functions (#7)

--- test/transaction.js
+++ test/transaction.js
@@ -23,6 +23,13 @@ exports.transaction = {
         test.ok(true);  // previous function didn't throw
         test.done();
     },
+    'can get a header': function (test) {
+        test.expect(1);
+        var newTrans = transaction.createTransaction();
+        newTrans.add_header('X-Test-Header', 'Has a value');
+        test.equal(newTrans.header.get('X-Test-Header'), 'Has a value');
+        test.done();
+    },
     'can call message_stream.add_line': function (test) {
         test.expect(1);
         var newTrans = transaction.createTransaction();
