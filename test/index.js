95661aeaf7f6f8f1d8679dc64d60347b5856329c
Matt Simerson <matt@tnpi.net>
2017-01-03 22:21:43 -0800

    Appveyor - windows compatibility (#11)
    * lint fixes
    * add appveyor.xml
    * replace posix / with path.join
    * use correct ordering of equal args
    * use path instead of posix / delimiters
    * remove grunt
    * add appveyor badge

--- test/index.js
+++ test/index.js
@@ -1,54 +1,56 @@
 'use strict';
 
+var path = require('path');
+
 var fixtures = require('../index');
 
 exports.exports = {
     'stub': function (test) {
         test.expect(1);
-        test.equal(typeof fixtures.stub.stub, 'function');
+        test.equal('function', typeof fixtures.stub.stub);
         test.done();
     },
     'logger': function (test) {
         test.expect(1);
-        test.equal(typeof fixtures.logger.loginfo, 'function');
+        test.equal('function', typeof fixtures.logger.loginfo);
         test.done();
     },
     'connection': function (test) {
         test.expect(1);
-        test.equal(typeof fixtures.connection.createConnection, 'function');
+        test.equal('function', typeof fixtures.connection.createConnection);
         test.done();
     },
     'transaction': function (test) {
         test.expect(1);
-        test.equal(typeof fixtures.transaction.createTransaction, 'function');
+        test.equal('function', typeof fixtures.transaction.createTransaction);
         test.done();
     },
     'line_socket': function (test) {
         test.expect(1);
-        test.equal(typeof fixtures.line_socket.connect, 'function');
+        test.equal('function', typeof fixtures.line_socket.connect);
         test.done();
     },
     'plugin': function (test) {
         test.expect(1);
-        var p = fixtures.plugin('test/fixtures/mock-plugin');
-        test.equal(typeof p.load_plugin, 'function');
+        var p = new fixtures.plugin(path.join('test', 'fixtures', 'mock-plugin'));
+        test.equal('function', typeof p.load_plugin);
         test.done();
     },
     'result_store': function (test) {
         test.expect(1);
         var rs = new fixtures.result_store();
         // console.log(rs);
-        test.equal(typeof rs.add, 'function');
+        test.equal('function', typeof rs.add);
         test.done();
     },
     'util_hmailitem': function (test) {
         test.expect(1);
-        test.equal(typeof fixtures.util_hmailitem.newMockHMailItem, 'function');
+        test.equal('function', typeof fixtures.util_hmailitem.newMockHMailItem);
         test.done();
     },
     'vm_harness': function (test) {
         test.expect(1);
-        test.equal(typeof fixtures.vm_harness.sandbox_require, 'function');
+        test.equal('function', typeof fixtures.vm_harness.sandbox_require);
         test.done();
     }
 }
